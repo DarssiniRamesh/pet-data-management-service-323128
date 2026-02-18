38243e5891476e4abe2197324fc37ff0ada81378
Kavia AI <production@kavia.ai>
2026-02-18 05:19:42 +0000

    Checkpoint 1 - cg251ceac4

---
+++ pets_backend/src/main/java/com/example/demo/petsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.petsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class petsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(petsbackendApplication.class, args);
+	}
+
+}
